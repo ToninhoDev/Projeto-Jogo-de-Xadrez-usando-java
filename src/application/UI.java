76e34854f8ec41fcef455bb6b11826f0a4058be1
ToninhoDev <toninho.dmas@protonmail.com>
2022-01-15 14:21:48 -0300

    Adicionado o metodo que atualiza a tela depois de uma jogada

--- src/application/UI.java
+++ src/application/UI.java
@@ -28,6 +28,11 @@ public class UI {
 	public static final String ANSI_PURPLE_BACKGROUND = "\u001B[45m";
 	public static final String ANSI_CYAN_BACKGROUND = "\u001B[46m";
 	public static final String ANSI_WHITE_BACKGROUND = "\u001B[47m";
+	
+	public static void clearScreen() {
+		System.out.print("\033[H\033[2J");
+		System.out.flush();
+	}
 
 	public static ChessPosition readChessPosition(Scanner sc) {
 		try {
@@ -42,7 +47,7 @@ public class UI {
 	}
 
 	public static void printBoard(ChessPiece[][] pieces) {
-		System.out.println("  a b c d e f g h");
+		
 		for (int i = 0; i < pieces.length; i++) {
 			System.out.print((8 - i) + " ");
 			for (int j = 0; j < pieces.length; j++) {
